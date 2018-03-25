ddfb2972ae7cbd94e90614430d49791797a64c57
JorgeCoke <jorge94ml@gmail.com>
2018-03-25 20:52:17 +0200

    ADD Game.prototype

--- script.js
+++ script.js
@@ -4,14 +4,34 @@
         var canvas = canvasElement.getContext('2d');
         var canvasSize = { x: canvasElement.width, y: canvasElement.height };
 
+        this.objects = [];
+        var self = this;
+
         var nextFrame = function () {
-            console.log(new Date());
+            self.update();
+            self.draw(canvas, canvasSize);
             requestAnimationFrame(nextFrame); //60fps
         };
 
         nextFrame();
     };
 
+    Game.prototype = {
+        update: function () {
+            console.log('update Objects');
+        },
+
+        draw: function (canvas, canvasSize) {
+            console.log('draw Objects');
+        },
+
+        addObject: function (obj) {
+            this.objects.push(obj);
+        }
+    };
+
+
+    // LOAD NEW GAME
     window.onload = function () {
         new Game();
     };
